b7b0b94ba228e41245f95cc57136a57f1bd6a46a
doman <domanski@basement.pl>
2023-11-03 19:26:29 +0100

    [fixes] too tired to write

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ alias dot="/usr/bin/git --git-dir=$HOME/.dot/ --work-tree=$HOME"
 dot config status.showUntrackedFiles no
 dot restore .
 
-mkdir -p Projects/[Personal,Work]
+mkdir -p Projects/{Personal,Work}
 SETUP_REPO_URL=git@github.com:ickyicky/setup.git
 git clone $SETUP_REPO_URL Projects/Personal/setup
 cd Projects/Personal/setup
