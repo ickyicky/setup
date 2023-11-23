2895acbade8018776acac88052e34b794310e68b
doman <domanski@basement.pl>
2023-11-23 11:03:59 +0100

    bump

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@
 cd $HOME
 REPO_URL=git@github.com:ickyicky/dot.git
 git clone --bare $REPO_URL $HOME/.dot/
-alias dot="/usr/bin/git --git-dir=$HOME/.dot/ --work-tree=$HOME"
-dot config status.showUntrackedFiles no
-dot restore .
+/usr/bin/git --git-dir=$HOME/.dot/ --work-tree=$HOME config status.showUntrackedFiles no
+/usr/bin/git --git-dir=$HOME/.dot/ --work-tree=$HOME restore --staged .
+/usr/bin/git --git-dir=$HOME/.dot/ --work-tree=$HOME restore .
 
 mkdir -p Projects/{Personal,Work}
 SETUP_REPO_URL=git@github.com:ickyicky/setup.git
