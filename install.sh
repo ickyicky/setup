8150607fc166cc12670e999d5630a0a1c6cb2465
Piotr Domański <pi.domanski@gmail.com>
2024-02-20 15:50:45 +0100

    structure change

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ git clone --bare $REPO_URL $HOME/.dot/
 /usr/bin/git --git-dir=$HOME/.dot/ --work-tree=$HOME restore --staged .
 /usr/bin/git --git-dir=$HOME/.dot/ --work-tree=$HOME restore .
 
-mkdir -p Projects/{Personal,Work}
+mkdir -p {Personal,Work}
 SETUP_REPO_URL=git@github.com:ickyicky/setup.git
-git clone $SETUP_REPO_URL Projects/Personal/setup
-cd Projects/Personal/setup
+git clone $SETUP_REPO_URL Personal/setup
+cd Personal/setup
 ./setup.sh
